95ce1d7e213ab0e941875344b3ebcc2eccf21cc2
Nabeel-nazeer <nabeelnazeer2565@gmail.com>
2023-06-23 14:08:41 +0300

    question 1

---
+++ 1.1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    if (n == 0)          //base case
+    return 1;
+    else
+    return n*factorial(n-1);     //recursive statement
+}
+
+int main()
+{
+    int f,n,choice;
+    do
+    {
+        cout<<"enter the number to find factorial : "<<endl;
+        cin>>n;
+        cout<<"factorial of "<<n <<" is "<< factorial(n);
+        cout<<endl;
+        cout<<"press 1 if you want to contiue\npress any number to exit"<<endl;
+        cin>>choice;
+
+
+    } while (choice == 1 );
+    
+}
