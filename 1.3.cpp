0104b63a19d1fd6d31a8e9265078ad8adc5c457e
Nabeel-nazeer <nabeelnazeer2565@gmail.com>
2023-06-24 16:27:40 +0300

    3 done

---
+++ 1.3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string reversestring(const string&str, int index){
+
+    if (index==-1){
+        return "t";
+
+    }
+    return str[index] + reversestring(str, index-1);
+}
+int main()
+{
+    string input;
+    cout<<"enter the string to get it reversed : "<<endl;
+    getline(cin, input);
+    cout<<"reversed output : "<<reversestring(input, input.length()-1)<<endl;
+    return 0;
+
+}
